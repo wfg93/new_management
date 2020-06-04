75218693d0a15bcfffded3ed9d09e0a91fea2545
wfg93 <1187449440@qq.com>
2020-06-04 11:43:11 +0800

    first commit

---
+++ src/components/navigation.vue
@@ -0,0 +1,96 @@
+<template>
+  <div class="nav">
+      <div class="navgat">
+         
+         <div class="navgtion">
+             <img src="../../static/img/maijilogo.jpg" alt="">
+         </div>
+         <div class="na_igton">
+             麦吉减肥法
+         </div>
+
+      </div>
+<div class="navgat">
+
+<div class="navgtion_a">
+<i class="ka_lio el-icon-message-solid"></i>
+</div>
+<div class="na_igton_b">
+昵称 <i class="tubiao el-icon-user"></i>
+</div>
+
+</div>
+
+  </div>
+</template>
+
+<script>
+export default {
+    name:"navigation",
+    data(){
+        return{
+
+        }
+    }
+
+}
+</script>
+
+<style scoped>
+.nav{
+    width: 100%;
+    height: 66px;
+    background: #ffffff;
+    position: fixed;
+    display: flex;
+    justify-content: space-between;
+}
+.navgat{
+    width: 230px;
+    height: 100%;
+    margin: 0 30px;
+    display: flex;
+    justify-content: space-between;
+}
+.navgat:nth-child(2){
+    width: 180px;
+}
+.navgtion{
+width: 80px;
+height: 67px;
+}
+img{
+    width: 100%;
+    height: 100%;
+}
+.na_igton{
+width: 115px;
+height: 60%;
+margin-top: 12px;
+line-height: 39px;
+font-size: 21px;
+font-weight: 600;
+color: #333333;
+/* font-family: Micr; */
+}
+.navgtion_a{
+width: 50px;
+height: 50px;
+margin-top: 8px;
+}
+.ka_lio{
+font-size: 27px;
+margin: 10px;
+color: #333333;
+}
+.na_igton_b{
+width: 80px;
+height: 50px;
+margin-top: 8px;
+line-height: 47px;
+color: #AAAAAA;
+}
+.tubiao{
+    margin-left: 15px;
+}
+</style>
---
+++ src/http/api.js
@@ -0,0 +1,68 @@
+import { apiServer } from './axios'
+import { apiServero } from './axios2'
+
+const baseURL = "http://118.190.106.118:8021/answer/manage" //公共域名
+    //const baseURL = "http://192.168.0.100:8021/answer/manage"
+const EData = (url, type = 'post') => (params) => apiServer[type](baseURL + url, params)
+const EDatar = (url, type = 'post') => (params) => apiServero[type](baseURL + url, params)
+
+
+//答题信息总览接口
+export const OVW_information = {
+    //展示的数据
+    ong: EData('/InformationOverview'),
+    //统计图
+    statistical: EData('/InformationOverviewEcharts'),
+    //答题题目
+    Answerquestions: EData('/listItemSort'),
+    //答题题目上下移动排序  
+    sortord: EDatar('/upOrDown'),
+    //答题小屏幕展示
+    respondent: EDatar('/queryItemById')
+}
+
+//奖金设置
+
+export const Bonus_set = {
+    //奖金设置展示数据
+    Query_the_bonus: EDatar('/queryBonusSetting'),
+    //账号统计
+    Account_statistics: EData('/withdrawAccount'),
+    //奖金设置完后提交
+    bonus_payment: EData('/bonusSetting')
+}
+
+//基础设置
+export const Base_Installation = {
+    //基础设置预览
+    Based_on_the_preview: EDatar('/queryBasicSetting'),
+    //基础设置提交
+    Set_the_BAse: EData('/basicSetting'),
+    //基础设置背景音乐
+    bacKGround_music: EData("/listMusics"),
+    //删除背景音乐
+    Delete_the_music: EDatar('/delMusic')
+
+}
+
+//答题设置
+export const The_answer_set = {
+    //答题预览
+    ANswer_to_preview: EDatar('/queryItemSetting'),
+    //答题提交
+    ANswer_sheet: EData("/answerSetting"),
+    //添加上传题目
+    ADD_the_Title: EData('/addItem'),
+    //题目渲染
+    the_title_number: EDatar('/queryItemBank'),
+    //题目修改
+    the_title_fix: EData('/editItem'),
+    //取消选中与不选中
+    selecte_title: EDatar('/markSelected'),
+    //题目删除
+    Delete_the_topic: EDatar('/delItem'),
+    //删除题目里面的图片
+    strikethrough: EData('/delPicture')
+
+
+}
